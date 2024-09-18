a7bf2ff96db785ba161fa74563e86d2ddd7c1892
MohammedRahmanSherif <mohammedrahman.optisol@gmail.com>
2024-09-18 18:59:46 +0530

    first commit

---
+++ src/main.py
@@ -0,0 +1,12 @@
+def main():
+    print(
+        '''
+                Hey Developer, Write me first! 😇
+
+                Contact Sherif, if needed any support. Thanks!
+                
+        '''
+    )
+
+if __name__ == "__main__":
+    main()
